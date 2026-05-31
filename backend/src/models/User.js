20d64e6586bc9eb1a747334f0d5ba2a95d1a4fed
kofiy3853.dot <kofiy3853@gmail.com>
2026-05-31 21:39:20 +0000

    Fix User pre-save hook: remove invalid next() usage

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -64,14 +64,14 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Encrypt password using bcrypt
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
+    // No next needed in async hook
 });
 
 // Sign JWT and return
